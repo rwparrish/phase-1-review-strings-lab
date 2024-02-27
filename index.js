a70ddf69892046b8d9f7deb01faf12b95aadd928
rwparrish <60946750+rwparrish@users.noreply.github.com>
2024-02-26 17:35:45 -0700

    pass first test

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+
+// define a currentUser variable using let or const
+// define and assign or reassign
+// I don't anticipate changing the value of the variable so I will use const instead of let
+
+const currentUser = 'Ryan'
+
+// concatenation
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!'
+
+// template literal with interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`
+
+console.log(currentUser.slice(0, 1))
